da49ec560b76ecf19215b0f41153fd3fb37cf8d4
shubham mishra <shubham.mishra@cuelogic.com>
2021-06-18 16:58:31 +0530

    gradle

---
+++ springgradle/src/test/java/com/springgradle/SpringgradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringgradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
